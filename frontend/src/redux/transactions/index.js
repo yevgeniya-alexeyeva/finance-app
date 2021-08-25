eaf98ca6df63e9c94a79d2eb9bc4d2251e58f6d4
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-08-25 20:38:27 +0200

    add use-effect in stat-tab not finish

--- frontend/src/redux/transactions/index.js
+++
@@ -1,3 +0,0 @@
-export { default as transactionsOperations } from './transactions-operations';
-export { default as transactionsSelectors } from './transactions-selectors';
-export { default as transactionsReducer } from './transactions-reducer';
