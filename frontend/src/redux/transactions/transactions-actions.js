13d09370dcb497c7eb6e2a065877e5f45132ed30
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-08-27 19:38:00 +0600

    Update transactions-actions.js

--- frontend/src/redux/transactions/transactions-actions.js
+++ frontend/src/redux/transactions/transactions-actions.js
@@ -4,9 +4,9 @@ export const filterTrRequest = createAction('transactions/filterTrRequest');
 export const filterTrSuccess = createAction('transactions/filterTrSuccess');
 export const filterTrError = createAction('transactions/filterTrError');
 
-export const fetchTrRequest = createAction('transactions/filterTrRequest');
-export const fetchTrSuccess = createAction('transactions/filterTrSuccess');
-export const fetchTrError = createAction('transactions/filterTrError');
+export const fetchTrRequest = createAction('transactions/fetchTrRequest');
+export const fetchTrSuccess = createAction('transactions/fetchTrSuccess');
+export const fetchTrError = createAction('transactions/fetchTrError');
 
 export const addTransactionRequest = createAction(
   'transactions/addTransactionRequest',
