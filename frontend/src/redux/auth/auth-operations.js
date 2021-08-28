adb496e63e0568b59ca1dbaa11535e09a9333b29
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-08-28 16:32:08 +0200

    add netlify and heroku url

--- frontend/src/redux/auth/auth-operations.js
+++ frontend/src/redux/auth/auth-operations.js
@@ -14,7 +14,7 @@ import {
   getCurrentUserError,
 } from './auth-actions';
 
-axios.defaults.baseURL = 'http://localhost:3000/';
+axios.defaults.baseURL = 'https://git.heroku.com/finance-app-wallet.git';
 
 const token = {
   set(token) {
