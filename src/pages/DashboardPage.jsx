69e4e09bfde08d1e1cb768366173a66fa48319a4
NadinShp <70705043+NadinShp@users.noreply.github.com>
2021-08-31 03:00:38 +0300

    add bgImg

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -48,38 +48,44 @@ const DashboardPage = () => {
       {matches => (
         <>
           <Header />
-          <Container>
-            <div className={styles.content}>
-              <div className={styles.walletContainer}>
-                <div className={styles.walletPanel}>
-                  <div>
-                    <MobileNavigation />
-                    {!matches.small ? <Balance /> : undefined}
+          <div className={styles.bgWrap}>
+            <Container>
+              <div className={styles.content}>
+                <div className={styles.walletContainer}>
+                  <div className={styles.walletPanel}>
+                    <div>
+                      <MobileNavigation />
+                      {!matches.small ? <Balance /> : undefined}
+                    </div>
+                    {!matches.small ? <Currency /> : undefined}
                   </div>
-                  {!matches.small ? <Currency /> : undefined}
                 </div>
-              </div>
 
-              {pathname.includes('home') && (
-                <div className={styles.btnAdd}>
-                  <AddTransaction />
-                </div>
-              )}
-              <Suspense fallback={<Loader />}>
-                <Switch>
-                  <Route exact path={routes.home} component={HomeAsync} />
-                  <Route exact path={routes.diagram} component={DiagramAsync} />
-                  {matches.small && (
+                {pathname.includes('home') && (
+                  <div className={styles.btnAdd}>
+                    <AddTransaction />
+                  </div>
+                )}
+                <Suspense fallback={<Loader />}>
+                  <Switch>
+                    <Route exact path={routes.home} component={HomeAsync} />
                     <Route
                       exact
-                      path={routes.currency}
-                      component={CurrencyAsync}
+                      path={routes.diagram}
+                      component={DiagramAsync}
                     />
-                  )}
-                </Switch>
-              </Suspense>
-            </div>
-          </Container>
+                    {matches.small && (
+                      <Route
+                        exact
+                        path={routes.currency}
+                        component={CurrencyAsync}
+                      />
+                    )}
+                  </Switch>
+                </Suspense>
+              </div>
+            </Container>
+          </div>
         </>
       )}
     </Media>
